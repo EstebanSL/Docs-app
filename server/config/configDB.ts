439e5e531596561be9e221c2dbd74da4326f24e1
EstebanSL <gandhisanchezl00@gmail.com>
2024-09-24 19:44:14 -0500

    Preparing deploy

--- server/config/configDB.ts
+++ server/config/configDB.ts
@@ -1,12 +1,12 @@
 // config/db.ts
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
  
 export const connectDB = () => {
-  const url = process.env.DB_CONNECTION_STRING;
+  const url = process.env.DB_CONNECTION_STRING!;
   
   try {
     mongoose.connect(url);
-  } catch (err) {
+  } catch (err: any) {
     console.log(err.message);
     process.exit(1);
   }
