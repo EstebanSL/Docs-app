d3efb4192db327f0de16c169f42f0c4f5191a5f9
EstebanSL <gandhisanchezl00@gmail.com>
2024-09-24 19:39:22 -0500

    Preparing deploy

--- server/server.ts
+++ server/server.ts
@@ -12,18 +12,19 @@ const {
 connectDB();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/documents", documents);
 
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const io = require("socket.io")(3001, {
+const io = require("socket.io")(process.env.SOCKET_PORT || 3001, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
